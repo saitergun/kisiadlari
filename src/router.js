d232caccbf69dfe746df1f25fb71012b13f96b31
sait ergün <tiasweb@gmail.com>
2020-05-24 23:07:52 +0300

    clean up router.js

--- src/router.js
+++ src/router.js
@@ -5,10 +5,30 @@ Vue.use(Router)
 
 export default new Router({
   base: process.env.BASE_URL,
+
   routes: [
-    { path: '*', component: () => import('./views/NotFound.vue') },
-    { path: '/', component: () => import('./views/Home.vue') },
-    { path: '/names', component: () => import('./views/Names.vue') },
-    { path: '/names/:id', component: () => import('./views/Name.vue'), props: route => ({ id: route.params.id }) }
+    {
+      path: '*',
+      name: 'NotFound',
+      component: () => import('./pages/NotFound.vue')
+    },
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('./pages/Home.vue')
+    },
+    {
+      path: '/names',
+      name: 'Names',
+      component: () => import('./pages/Names.vue')
+    },
+    {
+      path: '/names/:id',
+      name: 'Name',
+      component: () => import('./pages/Name.vue'),
+      props: route => ({
+        id: route.params.id
+      })
+    }
   ]
 })
